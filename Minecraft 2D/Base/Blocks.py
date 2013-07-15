a3fbb9404be7c7b4de654eedeefbcf4946e96768
Pew446 <mitch@tekket.net>
2013-07-15 12:57:42 -0600

    Added two block images, image class, etc

--- Minecraft 2D/Base/Blocks.py
+++ Minecraft 2D/Base/Blocks.py
@@ -1,25 +1,39 @@
+from Base.utils import *
+
 class BlocksManager:
     
     blocks = None
     
-    def __init__(self):
+    def __init__(self, pygame):
         self.blocks = (
-        Block(0, None, "Stone"),
-        Block(1, None, "Grass")
+        Block(0, Image(pygame.image.load("assets/blocks/stone.png"), (16, 16), (0,0)), "Stone"),
+        Block(1, Image(pygame.image.load("assets/blocks/grass.png"), (16, 16), (0,0)), "Grass")
         )
+        
+    def getBlockByName(self, name):
+        for b in self.blocks:
+            if(b.getName() == name):
+                return b
+        return None
+    
+    def getBlockById(self, bid):
+        for b in self.blocks:
+            if(b.getId() == bid):
+                return b
+        return None
 
 class Block:
-    id = None
+    bid = None
     image = None
     name = None
     
-    def __init__(self, id, image, name):
-        self.id = id
+    def __init__(self, bid, image, name):
+        self.bid = bid
         self.image = image
         self.name = name
     
     def getId(self):
-        return self.id
+        return self.bid
     
     def getImage(self):
         return self.image
