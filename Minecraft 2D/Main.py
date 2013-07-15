3afaec983dc3b2412c447af8f5232c607496016e
python09 <python09@LAB6-21.users.coe.utah.edu>
2013-07-15 13:10:56 -0600

    deleting Display class, moved commands to main

--- Minecraft 2D/Main.py
+++ Minecraft 2D/Main.py
@@ -3,7 +3,23 @@ Created on Jul 15, 2013
 
 @author: python10
 '''
-import Base/Display
+import pygame
+
+color = (0,0,0)
+
+def draw():
+    screen.fill(color)
+    for n in range(0, len(Block.ObjectArray)):
+        screen.blit(Block.ObjectArray[n][0], Block.gameObjectArray[n][1])
+                # Array Entry = file, position
+    for n in range(0, len(Entity.ObjectArray)):
+        screen.blit(Entity.ObjectArray[n][0], Entity.gameObjectArray[n][1])
+                # Array Entry = file, position
+        for n in range(0, len(Entity.ObjectArray)):
+        screen.blit(Entity.ObjectArray[n][0], Entity.gameObjectArray[n][1])
+                # Array Entry = file, position
+        pygame.display.flip()
 
 if __name__ == '__main__':
-    screen = Display.pywindow(0,0,'Minecraft 2-D')
+    pygame.display.set_caption('Minecraft 2D')
+    screen = pygame.display.set_mode((0,0), pygame.NOFRAME|pygame.FULLSCREEN)
