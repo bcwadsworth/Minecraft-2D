ce4e2215a9253d112f069b7c2afe4f5c8b7ce1ec
python05 <python05@LAB6-22.users.coe.utah.edu>
2013-07-18 13:25:38 -0600

    Added the title logo to the menu. Fixed the position of the splash message

--- Minecraft 2D/Renderers/Menu.py
+++ Minecraft 2D/Renderers/Menu.py
@@ -14,6 +14,10 @@ class Menu:
     optionsRect = None    
     quitButton = None
     quitRect = None
+    logo = None
+    logoRect = None
+    rotatedText = None
+    
     # the screen to draw on
     screen = None
     font = None
@@ -36,32 +40,46 @@ class Menu:
         return snippits[i]
     
     def __init__(self, surface, width, height):
-        pygame.init()
+        #load the images
         self.playButton = pygame.image.load('assets/GUI/playButton.png')
         self.optionsButton = pygame.image.load('assets/GUI/optionsButton.png')
-        self.quitButton = pygame.image.load('assets/GUI/quitButton.png')        
+        self.quitButton = pygame.image.load('assets/GUI/quitButton.png')
+        self.logo = pygame.image.load('assets/GUI/logo.png')
+        
+        #set up the screen  
         self.screen = surface
         self.width = width
         self.height = height
+        
+        # set up the picture rectangles
         self.quitRect = pygame.Rect((self.width - self.quitButton.get_rect().width)/2, self.height - self.quitButton.get_rect().height - self.buttonGap, self.quitButton.get_rect().width,
                                self.quitButton.get_rect().height)
         self.optionsRect = pygame.Rect(self.quitRect.x, self.quitRect.y - self.optionsButton.get_rect().height - self.buttonGap, self.optionsButton.get_rect().width, 
                                       self.optionsButton.get_rect().height)
         self.playRect = pygame.Rect(self.optionsRect.x, self.optionsRect.y - self.playButton.get_rect().height - self.buttonGap, self.playButton.get_rect().width,
-                                    self.playButton.get_rect().height)
+                                    self.playButton.get_rect().height)        
+        #scale logo
+        temp = self.logo
+        self.logo = pygame.transform.scale(temp, (width * 4/5, height/4))
+        self.logoRect = pygame.Rect((width - self.logo.get_rect().width)/2, (height - self.logo.get_rect().height)/4, self.logo.get_rect().width, self.logo.get_rect().height)
         
+        # set up the snippit
         self.font = pygame.font.SysFont('Arial', 32)        
         self.snippit = self.loadSnippit()     
-        self.snippitX = width/2
+        self.snippitX = width*2/3
         self.snippitY = height/6
+        self.rotate()
         
+    def rotate(self):
+        pic1 = self.font.render(self.snippit, 1, (255, 255, 0)) # save the text as an image
+        self.rotatedText = pygame.transform.rotate(pic1, self.rotateAngle) # rotate the image        
+               
     def draw(self):
         self.screen.blit(self.quitButton, (self.quitRect.x, self.quitRect.y))
         self.screen.blit(self.optionsButton, (self.optionsRect.x, self.optionsRect.y))
         self.screen.blit(self.playButton, (self.playRect.x, self.playRect.y))
-        pic1 = self.font.render(self.snippit, 1, (255, 255, 0)) # save the text as an image
-        rotatedPic = pygame.transform.rotate(pic1, self.rotateAngle) # rotate the image
-        self.screen.blit(rotatedPic, (self.snippitX, self.snippitY)) #show the image
+        self.screen.blit(self.logo, (self.logoRect.x, self.logoRect.y))
+        self.screen.blit(self.rotatedText, (self.snippitX, self.snippitY)) #show the image        
     
     #the event handler
     def getEvents(self, event):
